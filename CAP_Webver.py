5fd0924f3c5469a478422f46e610785fcd09e02c
yoshihrmt <yoshihrmt@gmail.com>
2025-05-30 20:59:50 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['Times New Roman', 'serif']
+plt.rcParams['font.family'] = ['serif']
 import matplotlib.lines as mlines
 from scipy.signal import find_peaks
 from scipy.integrate import simpson
@@ -12,7 +12,7 @@ import io
 st.markdown("""
 <style>
 html, body, [class*="css"]  {
-    font-family: "Times New Roman", Times, serif !important;
+    font-family: ,Times, serif !important;
 }
 </style>
 """, unsafe_allow_html=True)
