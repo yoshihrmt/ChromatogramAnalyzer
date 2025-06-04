21d8418ce9f7463429373b4ca507a71a7ca3ef7b
yoshihrmt <yoshihrmt@gmail.com>
2025-06-04 15:54:41 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -200,7 +200,8 @@ if uploaded_files and file_info_list:
         '', xy=(x_pos, y_start), xytext=(x_pos, y_start + scale_value),
         arrowprops=dict(arrowstyle='<->', linewidth=1)
     )
-    
+    # ここで一度デバッグ表示
+    st.write("font_scale_value:", font_scale_value)
     ax.text(
         x_pos + (current_xlim[1] - current_xlim[0]) * 0.01,
         y_start + scale_value / 2,
@@ -209,7 +210,6 @@ if uploaded_files and file_info_list:
         fontsize=font_scale_value,
         fontproperties=font_prop
     )
-
 
 
     st.pyplot(fig)
@@ -224,7 +224,7 @@ if uploaded_files and file_info_list:
         file_name="chromatogram.png",
         mime="application/png"
     )
-    st.write("スケールバー値font_scale_value:", font_scale_value)  # デバッグ用
+
     st.markdown("### 解析結果")
     for info in file_info_list:
         st.write(f"{info['legend']} … 検出されたピーク数: {len(info['peaks'])}")
