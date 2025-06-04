af3a716bfc8889df922564fefa2a46839ef676db
yoshihrmt <yoshihrmt@gmail.com>
2025-06-04 12:46:08 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = ['serif']
+plt.rcParams['font.serif'] = ['Times New Roman', 'Times', 'serif']
+plt.rcParams['mathtext.default'] = 'regular'
 import matplotlib.lines as mlines
 from scipy.signal import find_peaks
 from scipy.integrate import simpson
