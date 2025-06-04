e8b9bb1d729c16beeb5d9c9976131265d0126faa
yoshihrmt <yoshihrmt@gmail.com>
2025-06-04 13:36:36 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['serif']
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman', 'Times', 'serif']
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['axes.unicode_minus'] = False
 import matplotlib.lines as mlines
 from scipy.signal import find_peaks
 from scipy.integrate import simpson
