3f4b44f19ea98aabdf2faf33c5679969d9d35100
yoshihrmt <yoshihrmt@gmail.com>
2025-06-04 13:19:24 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = ['serif']
-plt.rcParams['font.serif'] = ['Yu Mincho', '游明朝', 'IPAMincho', 'MS Mincho', 'Noto Serif CJK JP', 'Noto Serif JP', 'Times New Roman', 'Times', 'serif']
-plt.rcParams['mathtext.default'] = 'regular'
 import matplotlib.lines as mlines
 from scipy.signal import find_peaks
 from scipy.integrate import simpson
