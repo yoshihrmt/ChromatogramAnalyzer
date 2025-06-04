c45e8fa604ae4153f26a5df702fc8b31bbb5a4b0
yoshihrmt <yoshihrmt@gmail.com>
2025-06-04 13:24:24 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['serif']
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = [
+    'Yu Mincho', '游明朝', 'MS Mincho', 'Hiragino Mincho ProN',
+    'Times New Roman', 'Times', 'serif'
+]
+plt.rcParams['axes.unicode_minus'] = False
 import matplotlib.lines as mlines
 from scipy.signal import find_peaks
 from scipy.integrate import simpson
