e650059e03c95972c8002de26f117cd6364d3624
yoshihrmt <yoshihrmt@gmail.com>
2025-07-25 09:56:41 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -327,11 +327,11 @@ if uploaded_files and file_info_list:
     arrow_x = 0 - (xlim[1] - xlim[0]) * 0.05  # x=0より左に5%オフセット
     arrow_y = 0 + (ylim[1] - ylim[0]) * 0.05  # y=0より上に5%オフセット   
    ax.annotate(
-    xy=(0, 0),             # 原点を指す
-    xytext=(arrow_x, arrow_y),  # テキストの位置（原点の左上）
-    arrowprops=dict(facecolor='black', shrink=0.05),
-    fontsize=10
-)
+        xy=(0, 0),             # 原点を指す
+        xytext=(arrow_x, arrow_y),  # テキストの位置（原点の左上）
+        arrowprops=dict(facecolor='black', shrink=0.05),
+        fontsize=10
+    )
 
     if show_legend:
         font_legend_prop = FontProperties(fname=font_path, size=font_legend)
