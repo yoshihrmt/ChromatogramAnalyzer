55df668db483ff6b60b673e9ac30e3f8b60df89f
yoshihrmt <yoshihrmt@gmail.com>
2025-07-25 10:30:00 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -288,7 +288,7 @@ if uploaded_files and file_info_list:
     xlim = ax.get_xlim()
 
     # 矢印を左上に移動（より確実な方法）
-    arrow_x = xlim[0] - (xlim[1] - xlim[0]) * 0.08
+    arrow_x = xlim[0] - (xlim[1] - xlim[0]) * 0.02
     arrow_y_start = ylim[0]
     arrow_y_end = ylim[1]
 
