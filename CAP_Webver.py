db5da110af87fab2e6b57136336ce79fc7005b1a
yoshihrmt <yoshihrmt@gmail.com>
2025-07-25 10:11:31 +0900

    Update CAP_Webver.py

--- CAP_Webver.py
+++ CAP_Webver.py
@@ -236,6 +236,7 @@ if uploaded_files and file_info_list:
     show_legend = st.checkbox("凡例を表示", value=True, key="show_legend_inline")
 
     fig, ax = plt.subplots(figsize=(9, 4))
+    fig.subplots_adjust(left=0.15, bottom=0.2, right=0.95, top=0.9)
     handles = []
 
     for idx, info in enumerate(file_info_list):
